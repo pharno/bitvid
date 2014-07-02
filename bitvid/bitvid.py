58b93c14b2e0b1843c66082cf11317bf63d15816
thecatspaw <philipp@bitvid.net>
2014-07-02 14:16:42 +0200

    return errors as json

--- bitvid/bitvid.py
+++ bitvid/bitvid.py
@@ -1,9 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from werkzeug.exceptions import default_exceptions
+from werkzeug.exceptions import HTTPException
 from shared import db
 
 import os
 
 app = Flask(__name__)
+
+def make_json_error(ex):
+    response = jsonify(message=str(ex))
+    response.status_code = (ex.code
+                            if isinstance(ex, HTTPException)
+                            else 500)
+    return response
+
+for code in default_exceptions.iterkeys():
+    app.error_handler_spec[None][code] = make_json_error
 
 db.app = app
 db.init_app(app)
