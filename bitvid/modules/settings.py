04072d2e4c47d3f8e709c9e0a2c75db334b4f214
MattB <mattb@bitvid.tv>
2014-07-05 11:52:26 -0700

    Stubbing settings module

---
+++ bitvid/modules/settings.py
@@ -0,0 +1,28 @@
+"""
+
+= Settings =
+
+== Video settings
+
+Video opens
+    Play immediately - *on/off
+    Start buffering - *on/off
+
+Quality
+    Main videos - *360p/480p/720p/1080p
+    Embedded videos - *360p/480p/720p/1080p
+    Preview videos - 40p/80p/*144p
+
+Zoom - *Normal/Crop to fit/Never stretch
+
+Controls - *Inside/Outside/Minimal/None
+
+Subtitles
+    Font - Arial/*Ubuntu
+
+Text color - Black/*White/Dark green/Green/Cyan/Blue/Orange/Yellow/Pink
+
+Outline color - *Black/White/Dark green/Green/Cyan/Blue/Orange/Yellow/Pink
+
+"""
+
