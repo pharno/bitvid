5431676b27251114e49264c73a3c59bfa6340cc9
thecatspaw <philipp@bitvid.net>
2014-07-05 23:53:57 +0200

    added pep8 fixer to the fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,7 +1,11 @@
 __author__ = 'pharno'
 
 from bitvid.bitvid import init_db as init_bitvid_db
+from fabric.operations import local
 
 
 def init_db():
     init_bitvid_db()
+
+def pep8():
+	local("autopep8 --aggressive --aggressive --in-place -r bitvid")
