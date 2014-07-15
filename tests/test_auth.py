dfab4a52eeab2c261b97a1e87532a03514916be1
thecatspaw <philipp@bitvid.net>
2014-07-15 22:22:38 +0200

    more serious authentication tests

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -9,5 +9,14 @@ class AuthTest(BaseTest):
     def _setup(self):
         self.client.register(self.email,self.password)
 
-    def test_something(self):
-        self.client.authenticate(self.email,self.password)
+    def test_authentication_correct(self):
+        assert self.client.authenticate(self.email,self.password)
+
+    def test_authentication_password_incorrect(self):
+        assert self.client.authenticate(self.email,"wrongpassword") == False
+
+    def test_authentication_email_incorrect(self):
+        assert self.client.authenticate("wrong@email.tv",self.password) == False
+
+    def test_totally_wrong_credentials(self):
+        assert self.client.authenticate("wrong@email.tv","wrongpassword") == False
