81edb2bf31a9dd00cd0a2a31bb1130bbd532595e
Philipp <philippa@bitvid.tv>
2014-09-10 20:48:47 +0200

    return 404 if the the ES index doesnt exist yet

--- bitvid/modules/searchModule.py
+++ bitvid/modules/searchModule.py
@@ -1,15 +1,18 @@
 from bitvid.shared import get_es, get_es_index
 from bitvid.lib import BitVidRestful
+from bitvid.errors import NotFound
 
 from flask import request
 from pprint import pformat
-
+import pyelasticsearch
 
 class SearchResource(BitVidRestful.BitVidRestResource):
     def get(self):
         searchquery = request.args.get("q")
-
-        seachresult = get_es().search(searchquery, index=get_es_index())
+        try:
+            seachresult = get_es().search(searchquery, index=get_es_index())
+        except pyelasticsearch.exceptions.ElasticHttpNotFoundError:
+            raise NotFound
 
         actualhits = [hit["_source"] for hit in seachresult["hits"]["hits"]]
         # return pformat(actualhits, indent=4)
