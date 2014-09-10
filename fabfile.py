f5e8d4d3f3fc9e429f04490235084c41f3167a22
Philipp <philippa@bitvid.tv>
2014-09-10 18:34:00 +0200

    fab reset command. resets the database and deletes the ES index

--- fabfile.py
+++ fabfile.py
@@ -8,5 +8,10 @@ def init_db():
     init_bitvid_db()
 
 
+def reset():
+    local("rm database.sqlite")
+    local("curl -XDELETE http://localhost:9200/bitvid")
+    init_db()
+
 def pep8():
     local("autopep8 --aggressive --aggressive --in-place -r bitvid")
