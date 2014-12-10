f7fd40ad9c9b7b6128cbd2b284e403ee7cc863d0
Philipp <philippa@bitvid.tv>
2014-12-10 20:20:38 +0100

    run celery if env[WORKER] is set

--- application.py
+++ application.py
@@ -4,5 +4,7 @@ import os
 
 from bitvid.bitvid import app as application
 
-if __name__ == '__main__':
+if __name__ == '__main__' and not application.config["WORKER"]:
     application.run(os.environ.get("HOST","127.0.0.1"))
+else:
+    os.system("celery -A bitvid.tasks worker -l debug")
