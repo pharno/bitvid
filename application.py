9a7e009e1bfd75e721d9fecd31ad720dab8e152f
Philipp <philippa@bitvid.tv>
2015-01-06 18:21:09 +0100

    stop backend from starting as worker

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ import os
 
 from bitvid.bitvid import app as application
 
-if __name__ == '__main__' and not application.config["WORKER"]:
+if __name__ == '__main__' and not application.config["WORKER"] == True:
     application.run(os.environ.get("HOST","127.0.0.1"))
 else:
     os.system("celery -A bitvid.tasks worker -l debug")
